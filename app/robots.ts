345b371f961e411ce478c1e4816737afd3581805
TSunDanceK <sonnybrindle@hotmail.com>
2026-03-27 21:15:24 +0000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
   allow: "/",
   disallow: ["/api/"],
 },
-    sitemap: "https://mystockharbor.com/sitemap.xml",
+    sitemap: "https://www.mystockharbor.com/sitemap.xml",
   };
 }
  
