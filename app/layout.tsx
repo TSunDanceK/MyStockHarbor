950a0f1fbfe10997b706fafb7730dd4969f4aa24
TSunDanceK <sonnybrindle@hotmail.com>
2026-03-31 07:05:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
   description:
     "MyStockHarbor (My Stock Harbor) helps traders track stocks, analyse technical indicators, monitor market benchmarks, and learn trading strategies with free educational tools, stock screeners, and market insights.",
   icons: {
-    icon: "/logo.png",
-    shortcut: "/logo.png",
-    apple: "/logo.png",
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 
   openGraph: {
