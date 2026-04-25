096393a8d455ee7edeee4ce43813d76de8813e29
TSunDanceK <sonnybrindle@hotmail.com>
2026-04-25 07:22:29 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
    rules: {
   userAgent: "*",
   allow: "/",
-  disallow: ["/api/"],
+disallow: ["/api/", "/*?symbol="],
 },
     sitemap: "https://www.mystockharbor.com/sitemap.xml",
   };
