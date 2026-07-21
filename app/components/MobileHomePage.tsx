7a389878dc79f5c57e6c74e4719cb0790b6f01e7
TSunDanceK <sonnybrindle@hotmail.com>
2026-07-21 14:26:45 +0100

    Disable prefetch on mobile nav tile and footer Links
    Prevents one background prefetch request per tile in the 10-tile mobile nav grid and per footer link, which was inflating real request counts and skewing rate-limit accounting.

--- app/components/MobileHomePage.tsx
+++ app/components/MobileHomePage.tsx
@@ -281,6 +281,7 @@ export default function MobileHomePage() {
             <Link
               key={tile.label}
               href={resolvedHref}
+              prefetch={false}
               style={{
                 display: "flex",
                 flexDirection: "column",
@@ -347,6 +348,7 @@ export default function MobileHomePage() {
           <Link
             key={l.href}
             href={l.href}
+            prefetch={false}
             style={{ color: "inherit", textDecoration: "none" }}
           >
             {l.label}
