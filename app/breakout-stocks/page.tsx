d69446fb1aac8bfef2c462a80a746f8b93791837
TSunDanceK <sonnybrindle@hotmail.com>
2026-08-04 21:48:41 +0100

    chunk 10: breakout stocks guide CTA retarget

--- app/breakout-stocks/page.tsx
+++ app/breakout-stocks/page.tsx
@@ -469,7 +469,7 @@ export default function BreakoutStocksPage() {
             }}
           >
             <Link
-              href="/pickers"
+              href="/breakout-signal-stocks"
               style={{
                 display: "inline-flex",
                 alignItems: "center",
@@ -486,7 +486,7 @@ export default function BreakoutStocksPage() {
                 whiteSpace: "nowrap",
               }}
             >
-              Browse Stock Ideas →
+              See Breakout Stocks Now →
             </Link>
 
             <Link
@@ -774,5 +774,5 @@ const paragraphStyle: React.CSSProperties = {
 const inlineLinkStyle: React.CSSProperties = {
   color: "#60a5fa",
   fontWeight: 800,
-  textDecoration: "none",
+  textDecoration: "none";
 };
